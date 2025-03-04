c687893e670cd2bda69c9a04ad3cd9fb5c8b5f35
Sufi <120452154+MarcalekM@users.noreply.github.com>
2025-03-04 10:25:59 +0100

    Done

--- Kutya.cs
+++ Kutya.cs
@@ -17,30 +17,34 @@ namespace KutyaGyak
         public int Kor { get; private set; }
         public string Nev { get; private set; }
 
-        static public List<Kutya> ListaFeltoltes(string forras)
+        public override string ToString()
+        {
+            return $"\tNév\t\t: {Nev}\n\tKor\t\t: {Kor}\n\tGazda\t\t: {Gazda.Nev}\n\tChipellés dátuma: {ChipDatum.Year}.{ChipDatum.Month}.{ChipDatum.Day}.";
+        }
+
+        static public List<Kutya> LoadFromCsv(string forras)
         {
             List<Kutya> kutyaLista = new();
             string path = $@"../../../src/{forras}";
             using StreamReader sr = new(
                 path: path,
                 encoding: UTF8Encoding.UTF8);
-            while (!sr.EndOfStream)
-            {
-                
-            }
+            _ = sr.ReadLine();
+            while (!sr.EndOfStream) kutyaLista.Add(new(sr.ReadLine()));
             return kutyaLista;
         }
 
-        public Kutya(int id, string nev, bool kan, int kor, DateTime chipDatum, string kepUrl, Fajta fajta, Gazda gazda)
+        public Kutya(string sor)
         {
-            Id = id;
-            Nev = nev;
-            Kan = kan;
-            Kor = kor;
-            ChipDatum = chipDatum;
-            KepUrl = kepUrl;
-            Fajta = fajta;
-            Gazda = gazda;
+            var temp = sor.Split(';');
+            Id = int.Parse(temp[0]);
+            Nev = temp[1];
+            Kan = temp[2] == "1";
+            Kor = int.Parse(temp[8]);
+            ChipDatum = DateTime.Parse(temp[9]);
+            KepUrl = temp[10];
+            Fajta = new Fajta(int.Parse(temp[3]), temp[4]);
+            Gazda = new Gazda(int.Parse(temp[5]), temp[6], temp[7]);
         }
     }
 }
--- Program.cs
+++ Program.cs
@@ -1,3 +1,20 @@
 ﻿using KutyaGyak;
 
-List<Kutya> kutyak = Kutya.ListaFeltoltes("kutyak.csv");
+List<Kutya> kutyak = Kutya.LoadFromCsv("adatok.csv");
+
+var f6 = kutyak.Average(k =>k.Kor);
+Console.WriteLine($"6. feladat: Kutyák átlagéletkora: {f6:0.00} év\n");
+
+
+var f7 = kutyak.Where(k => k.Kan)
+    .OrderByDescending(k => k.Kor)
+    .First();
+Console.WriteLine($"7. feladat: A legidősebb kan kutya neve és fajtája:\n{f7.ToString()}\n");
+
+Console.WriteLine("8. feladat: Adatok fájlba írása");
+var f8 = kutyak.Where(k => k.ChipDatum.Year.Equals(2022))
+    .ToList();
+using StreamWriter sw = new(
+    path: @"../../../src/idei.txt",
+    append: false);
+foreach (var kutya in f8)  sw.WriteLine($"{f8.IndexOf(kutya) + 1};{kutya.Nev};{kutya.ChipDatum.Year}.{kutya.ChipDatum.Month}.{kutya.ChipDatum.Day}.");
